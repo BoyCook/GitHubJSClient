52b089400c1b0c87a6bf8a02f07dd0f57567613e
Craig Cook <boycook@me.com>
2013-03-04 16:26:54 +0000

    Added GitHub.getUserEvents - put JSON parsing back into response callback as storing state

--- test/spec/GitHubSpec.js
+++ test/spec/GitHubSpec.js
@@ -3,7 +3,7 @@ var GitHub = require('../../lib/github').GitHub;
 describe('GitHub', function () {
     var gitHub;
     var error = function (err, response, body) {
-        console.log('ERROR [%s]', err);
+        console.log('ERROR [%s] - [%s] - [%s]', err, response, body);
         done();
     };
 
@@ -16,7 +16,7 @@ describe('GitHub', function () {
     it('should get users repos', function (done) {
         gitHub.getUsersRepos('BoyCook', error,
             function (data) {
-                expect(JSON.parse(data).length).toEqual(28);
+                expect(data.length).toEqual(28);
                 done();
             }
         );
@@ -25,7 +25,16 @@ describe('GitHub', function () {
     it('should get users gists', function (done) {
         gitHub.getUsersGists('BoyCook', error,
             function (data) {
-                expect(JSON.parse(data).length).toEqual(7);
+                expect(data.length).toEqual(7);
+                done();
+            }
+        );
+    });
+
+    it('should get users events', function (done) {
+        gitHub.getUsersEvents('BoyCook', error,
+            function (data) {
+                expect(data.length).toEqual(30);
                 done();
             }
         );
